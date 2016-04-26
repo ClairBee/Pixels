fa977b3504da39582e823411d23664d5b0b01e74
ClairBee <clair.with.no.e@gmail.com>
2016-04-26 16:32:33 +0100

    16.04.26: notes on identification of spots on beryllium screen.

--- R-code/16-04-21-screen-damage.R
+++ R-code/16-04-21-screen-damage.R
@@ -1,9 +1,14 @@
 
-# identifying blobs on screen based on morphology
+# UPDATE HISTOGRAMS TO SHOW 99% QUANTILE FOR BRIGHT & DIM PIXELS
+# ADD HISTOGRAMS OF GREY IMAGES
+
+# WRITE UP IDENTIFICATION OF NON-RESPONSIVE PIXELS
+# ADD NON-RESPONSIVE PIXELS TO SUPERCLUSTERS
+
+####################################################################################################
+# IDENTTIFICATION OF SPOTS ON SCREEN BASED ON MORPHOLOGY
 
 library("IO.Pixels")
-
-library(mmand)      # needed for morphological operations
 
 fpath <- "./Notes/Spots-on-screen/fig/"
 load.pixel.means()
@@ -244,21 +249,41 @@ panels <- panel.lm(spot.res, "x + y")
 res <- spot.res - panels$fitted.values
 
 # plot raw residuals with Johnson fit
-hist(res, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), main = "Johnson fit to all residuals")
+pdf(paste0(fpath, "res-all.pdf"))
+hist(res, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), 
+            yaxt = "n", main = "", xlab = "", ylab = "")
 lines(c(-50000:22900), dJohnson(c(-50000:22900), JohnsonFit(res)), col = "red", lwd = 2)
 
+rect(-50000, 0, qJohnson(0.001, JohnsonFit(res)), 0.01, col = adjustcolor("skyblue", alpha = 0.2), border = "cyan3")
+rect(-50000, 0, qJohnson(0.0001, JohnsonFit(res)), 0.01, col = adjustcolor("green2", alpha = 0.2), border = "orange")
+rect(qJohnson(0.999, JohnsonFit(res)), 0, 20000, 0.01, col = adjustcolor("gold", alpha = 0.2), border = "cyan3")
+rect(qJohnson(0.9999, JohnsonFit(res)), 0, 20000, 0.01, col = adjustcolor("orange", alpha = 0.2), border = "orange")
+dev.off()
+
 # plot residuals + Johnson fit without dim spots
+pdf(paste0(fpath, "res-without-spots.pdf"))
 res.adj <- res[xyFromCell(zz, which(is.na(getValues(zz))))]
-hist(res.adj, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), main = "Johnson fit without dim spots")
+hist(res.adj, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), main = "", xlab = "", ylab = "")
 lines(c(-50000:22900), dJohnson(c(-50000:22900), JohnsonFit(res.adj)), col = "red", lwd = 2)
+rect(-50000, 0, qJohnson(0.001, JohnsonFit(res.adj)), 0.01, col = adjustcolor("skyblue", alpha = 0.2), border = "cyan3")
+rect(-50000, 0, qJohnson(0.0001, JohnsonFit(res.adj)), 0.01, col = adjustcolor("green2", alpha = 0.2), border = "orange")
+rect(qJohnson(0.999, JohnsonFit(res.adj)), 0, 20000, 0.01, col = adjustcolor("gold", alpha = 0.2), border = "cyan3")
+rect(qJohnson(0.9999, JohnsonFit(res.adj)), 0, 20000, 0.01, col = adjustcolor("orange", alpha = 0.2), border = "orange")
+dev.off()
 
 # plot residuals + Johnson fit without dim spots, without edge boundary
+pdf(paste0(fpath, "res-cropped-without-spots.pdf"))
 res.adj.c <- res[xyFromCell(crop(zz, c(10.5, 1985.5, 10.5, 1985.5)), 
                             which(is.na(getValues(crop(zz, c(10.5, 1985.5, 10.5, 1985.5))))))]
-hist(res.adj.c, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), main = "Johnson fit without dim spots")
+hist(res.adj.c, breaks = "fd", prob = T, ylim = c(0, 2e-06), xlim = c(-10000,10000), main = "", xlab = "", ylab = "")
 lines(c(-50000:22900), dJohnson(c(-50000:22900), JohnsonFit(res.adj.c)), col = "red", lwd = 2)
+rect(-50000, 0, qJohnson(0.001, JohnsonFit(res.adj.c)), 0.01, col = adjustcolor("skyblue", alpha = 0.2), border = "cyan3")
+rect(-50000, 0, qJohnson(0.0001, JohnsonFit(res.adj.c)), 0.01, col = adjustcolor("green2", alpha = 0.2), border = "orange")
+rect(qJohnson(0.999, JohnsonFit(res.adj.c)), 0, 20000, 0.01, col = adjustcolor("gold", alpha = 0.2), border = "cyan3")
+rect(qJohnson(0.9999, JohnsonFit(res.adj.c)), 0, 20000, 0.01, col = adjustcolor("orange", alpha = 0.2), border = "orange")
+dev.off()
 
-
+hist(pw.m[,,"white", "160314"], breaks = "fd", prob = T, ylim = c(0, 2e-06))
 # q-q plots to compare
 {
     plot(qJohnson(c(1:999)/1000, JohnsonFit(res)), 
@@ -303,7 +328,7 @@ ks.test(res, pJohnson, JohnsonFit(res.adj.c))           # D = 0.014879
 # takes around 4.5 minutes to run over all 11 images
 spots <- lapply(dimnames(pw.m)[[4]], screen.spots)
 
-cols <- c("orangered2", "orange", "gold", "green", "chartreuse3", "cyan3", "black", "black",
+cols <- c("orangered2", "orange", "gold", "green", "chartreuse3", "cyan3", "seagreen", "darkseagreen",
           "skyblue", "blue", "magenta2")
 
 pdf(paste0(fpath, "All-screen-spots.pdf"))
@@ -346,13 +371,91 @@ zz <- screen.spots("150529")
 qq <- t(as.matrix(zz, ncol = 1996)[1996:1,])
 image(qq, col = "magenta3")
 
+# MATCH DIM PATCHES IN SUCCESSIVE IMAGES, PLOT PROGRESSION MORE CLEARLY                         ####
+spots <- lapply(dimnames(pw.m)[[4]], screen.spots)
+
+sc <- list()
+for (sp in 1:length(spots)) {
+    sc[[sp]] <- ddply(data.frame(xyFromCell(spots[[sp]], which(!is.na(getValues(spots[[sp]])))),
+                                 id = getValues(spots[[sp]])[!is.na(getValues(spots[[sp]]))]),
+                      .(id), summarise, 
+                      xm = round(mean(x),0), ym = round(mean(y),0),
+                      count = length(x), 
+                      width = max(x) - min(x), height = max(y) - min(y), n = sp)
+}
+
+#--------------------------------------------------------------------------------------
+qq <- do.call("rbind", sc)
+
+dt <- apply(cbind(lapply(dimnames(pw.m)[[4]], substring, 1, 2), "-",
+                  lapply(dimnames(pw.m)[[4]], substring, 3, 4), "-", 
+                  lapply(dimnames(pw.m)[[4]], substring, 5, 6)), 1, paste, collapse = "")
+
+lt.cols <- c("BrickRed", "Orange", "gold", "SpringGreen", "ForestGreen", "Turquoise", "Black", "Black", 
+             "ProcessBlue", "Blue", "Magenta")
+
+summ <- data.frame(dt = dt[count(qq, 'n')$n],
+                   nspots = count(qq, 'n')$freq, 
+                   px = aggregate(count ~ n, qq, 'sum')$count,
+                   prop = round(aggregate(count ~ n, qq, 'sum')$count / (1996^2) * 100, 2),
+                   legend = apply(cbind("\\textcolor{", lt.cols[count(qq, 'n')$n], "}{$\\bullet$}"), 1, paste, collapse = ""))
+
+write.csv(summ, paste0(fpath, "spot-summary.csv"), row.names = F, quote = F)
+           
+#--------------------------------------------------------------------------------------
+library(stringdist)
+df[which.min(Reduce(`+`,Map(stringdist,df, newdata,
+                            method='jaccard'))),]
+
+df <- sc[[1]]
+newdata <- sc[[2]][2,]
+
+zz <- which.min(Reduce('+', Map(stringdist, df, newdata, method = 'jaccard')))
+
+Map(stringdist, df, newdata, method = 'jaccard')
+
+which.min(Reduce("+", Map(stringdist, sc[[1]][, c("xm", "ym")], sc[[2]][2, c("xm", "ym")])))
+
+# get closest match by x & y distance
+match <- data.frame(x = unlist(lapply(lapply(lapply(sc[[2]][, c("xm")], '-', sc[[1]][, c("xm")]), abs), which.min)),
+                    y = unlist(lapply(lapply(lapply(sc[[2]][, c("ym")], '-', sc[[1]][, c("ym")]), abs), which.min)))
+
+#--------------------------------------------------------------------------------------------------
+# alternative approach: get nearest-neighbour distance of all points, by height, width & freq
+nn <- data.frame(sc[[2]][,c("xm", "ym")],
+                 sc[[1]][knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1), c("xm", "ym")])
+    
+# find image offset vs first image
+sc[[2]]$xm - sc[[1]]$xm[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)]
+sc[[2]]$ym - sc[[1]]$ym[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)]
+
+cbind(knnx.dist(sc[[1]][, -1], sc[[2]][, -1], k = 1),
+      sc[[2]]$xm - sc[[1]]$xm[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)],
+      sc[[2]]$ym - sc[[1]]$ym[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)])
+
+
+cols <- c("orangered2", "orange", "gold", "green", "chartreuse3", "cyan3", "black", "black",
+          "skyblue", "blue", "magenta2")
+
+image(c(1:1996), c(1:1996), t(as.matrix(spots[[1]], ncol = 1996)[1996:1,]),
+      col = adjustcolor(cols[1], alpha = 0.5), asp = T, xlab = "", ylab = "")
+
+nn.dist <- data.frame(x = sc[[2]]$xm - sc[[1]]$xm[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)],
+                      y = sc[[2]]$ym - sc[[1]]$ym[knnx.index(sc[[1]][, -1], sc[[2]][, -1], k = 1)])
+sc[[2]][,"id"]
+
+findInterval(nn.dist$x, quantile(nn.dist$x, c(0.25, 0.75)))
+findInterval(nn.dist$y, quantile(nn.dist$y, c(0.25, 0.75)))
+
+####################################################################################################
+
 # RANDOM BAD SPOT NOTICED IN WHITE IMAGES                                                       ####
 focus <- matrix(c(1105 + rep(c(-3:3), 7), 490 + sort(rep(c(-3:3), 7))), ncol = 2)
 plot(0, type = "n", xlim = range(focus[,1]), ylim = range(focus[,2]), xlab = "", ylab = "", main = "white images")
 text(focus, cex = 0.7, labels = round(pw.m[,,"white", "150828"][focus]/1000,0))
 
 plot(0, type = "n", xlim = range(focus[,1]), ylim = range(focus[,2]), xlab = "", ylab = "", main = "grey images")
-text(focus, cex = 0.7, labels = round(pw.m[,,"black", "150828"][focus]/1000,0))
+text(focus, cex = 0.7, labels = round(pw.m[,,"grey", "150828"][focus]/1000,0))
 
 plot(0, type = "n", xlim = range(focus[,1]), ylim = range(focus[,2]), xlab = "", ylab = "", main = "black images")
 text(focus, cex = 0.7, labels = round(pw.m[,,"black", "150828"][focus]/1000,0))
@@ -366,6 +469,14 @@ bn <- qJohnson(0.999, JohnsonFit(pw.m[,,"black", "150828"]))
 un.g <- which(matrix(findInterval(pw.m[,,"grey", "150828"], bn), ncol = 1996) == 0, arr.ind = T)
 un.w <- which(matrix(findInterval(pw.m[,,"white", "150828"], bn), ncol = 1996) == 0, arr.ind = T)
 
-points(un.g, pch = 16, col = adjustcolor("purple", alpha = 0.5), cex = 3)
-points(un.w, pch = 16, col = adjustcolor("purple", alpha = 0.5), cex = 3)
+points(un.g, pch = 16, col = adjustcolor("blue", alpha = 0.5), cex = 3)
+points(un.w, pch = 16, col = adjustcolor("red", alpha = 0.5), cex = 3)
 
+# ALTERNATIVE THRESHOLDING                                                                      ####
+
+    # hot - max value in black images
+    # v. bright - cutoff halfway between mean (median?) & upper limit
+    # bright - cutoff halfway between mean (median?) & 'hot' threshold
+    # slightly bright - use quantile cutoff
+
+    # dim pixels - what's left after excluding edges, dim spots & non-responsive?
