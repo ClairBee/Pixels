ca400686a8fc5cbd60422de8e9cf9baed6b3a6cf
ClairBee <clair.with.no.e@gmail.com>
2016-06-10 16:58:51 +0100

    16.06.09: work on identifying common trajectories through state space. Functions now completed, remaining analysis will hopefully be completed on Monday

--- R-code/16-06-09-final-classifications.R
+++ R-code/16-06-09-final-classifications.R
@@ -14,59 +14,140 @@ md.g <- readRDS("./Other-data/Median-diffs-grey.rds")
 
 ####################################################################################################
 
+# INCLUDE A NOTE ON LOOKING FOR DIM & HORIZONTAL LINES, THEN FILTERING MANUALLY
+    # dim line segments may appear if there are two v bright points adjacent to it within a short distance of one another
+    # use 
+# WHAT IS TARGET VALUE OF AIR, AND HOW IS THIS RELATED TO GREY MEAN VALUE? IS GREY REPRESENTATIVE?
+
+####################################################################################################
+
+# COMMON ELEMENTS OF BAD PIXEL MAP                                                              ####
+
+# bad pixel types that don't change with thresholds: 
+    # edges, non-responsives, abs. hot/dead, screen spots
+{
+    bp.const <- apply(pw.m, 4,
+                      function(acq) rbind(data.frame(edge.px(acq), type = "edge"),
+                                          data.frame(no.response(bright.image = acq[,,"grey"], dark.image = acq[,,"black"]), type = "no.resp"),
+                                          data.frame(which(acq[,,"black"] == 65535, arr.ind = T), type = "hot"),
+                                          data.frame(which(acq[,,"white"] == 0, arr.ind = T), type = "dead"),
+                                          data.frame(screen.spots(acq[,,"white"]), type = "screen.spot")))
+    saveRDS(bp.const, paste0(fpath, "bad-px-constant.rds"))
+}
+
+# lines (may want to observe these pixels singly, so keep separate)
+{
+    bp.lines <- apply(pw.m, 4,
+                      function(acq) data.frame(which(find.lines(acq[, , "black"]) > 0, arr.ind = T), type = "line.b"))
+    saveRDS(bp.lines, paste0(fpath, "bad-px-bright-lines.rds"))
+    
+    # no useful dim columns, or row artefacts of either kind.
+        qq <- apply(pw.m, 4, function(acq) which(find.lines(acq[, , "black"], dim.lines = T) > 0, arr.ind = T))
+        qq <- apply(pw.m, 4, function(acq) which(find.lines(acq[, , "black"], horizontal = T) > 0, arr.ind = T))
+        qq <- apply(pw.m, 4, function(acq) which(find.lines(acq[, , "black"], dim.lines = T, horizontal = T) > 0, arr.ind = T))
+}
+
+# locally dim & bright pixels
+{
+    bp.local <- lapply(names(md.b),
+                       function(dt) rbind(data.frame(which(md.b[[dt]] > 2 * mad(pw.m[,,"black", dt]), arr.ind = T),
+                                                     type = "l.bright"),
+                                          data.frame(which(md.b[[dt]] < -2 * mad(pw.m[,,"black", dt]), arr.ind = T),
+                                                     type = "l.dim"),
+                                          data.frame(which(md.g[[dt]] > 2 * mad(pw.m[,,"grey", dt]), arr.ind = T),
+                                                     type = "l.bright"),
+                                          data.frame(which(md.g[[dt]] < -2 * mad(pw.m[,,"grey", dt]), arr.ind = T),
+                                                     type = "l.dim")))
+    saveRDS(bp.local, paste0(fpath, "bad-px-local-bright-dim-pixels.rds"))
+}
+
+# function to combine & rationalise partial bad pixel maps
+merge.bp.lists <- function(bp.list, cat.order) {
+    
+    bpx <- list()
+    for (i in 1:length(bp.list[[1]])) {
+        bpx[[i]] <- rbind.fill(lapply(bp.list, "[[", i))
+        bpx[[i]]$type = ordered(bpx[[i]]$type, levels = cat.order)
+        bpx[[i]] <- bpx[[i]][order(bpx[[i]]$type),]
+        bpx[[i]] <- bpx[[i]][!duplicated(bpx[[i]][,1:2]),]
+    }
+    names(bpx) <- names(bp.list[[which.max(lapply(lapply(bp.list, names), length))]])
+    return(bpx)
+}
+
+####################################################################################################
+
 # MEAN-VALUED CLASSIFICATION                                                                    ####
     # this is the 'standard' classification approach used throughout the project so far.
     # simplest approah - use as baseline against which to look for improvements.
 
 # define possible bad pixel states 
-Cat <- c("no.resp", "dead", "hot", "v.bright", "bright", "line.b", "l.bright", "screen spot", "line.d", "edge", "v.dim", "dim", "l.dim")
+Cat <- c("no.resp", "dead", "hot", "v.bright", "bright", "line.b", "edge", "l.bright", "line.d", "screen.spot", "v.dim", "dim", "l.dim")
 Cat.cols <- c("purple", "black", "magenta3", "red", "orange", "gold", "yellow", "grey", "violet", NA, "green3", "lightskyblue", "grey")
 
 
-# classify bad pixels
+# classify bad pixels by global thresholding
 {
-    bp <- apply(pw.m, 4,
-                function(acq) rbind(data.frame(edge.px(acq), type = ordered("edge", levels = Cat)),
-                                    data.frame(no.response(bright.image = acq[,,"grey"], dark.image = acq[,,"black"]), type = "no.resp"),
-                                    data.frame(which(acq[,,"black"] == 65535, arr.ind = T), type = "hot"),
-                                    data.frame(which(acq[,,"white"] == 0, arr.ind = T), type = "dead"),
-                                        data.frame(which(acq[,,"white"] > median(acq[,,"white"]) + (65535 - median(acq[,,"white"])) / 2, arr.ind = T), type = "v.bright"),
-                                        data.frame(which(acq[,,"white"] > median(acq[,,"white"]) + (65535 - median(acq[,,"white"])) / 4, arr.ind = T), type = "bright"),
-                                        data.frame(which(acq[,,"white"] < median(acq[,,"white"]) / 2, arr.ind = T), type = "v.dim"),
-                                        data.frame(which(acq[,,"white"] < median(acq[,,"white"]) * 0.75, arr.ind = T), type = "dim"),
-                                    data.frame(which(acq[,,"grey"] > median(acq[,,"grey"]) + (65535 - median(acq[,,"grey"])) / 2, arr.ind = T), type = "v.bright"),
-                                    data.frame(which(acq[,,"grey"] > median(acq[,,"grey"]) + (65535 - median(acq[,,"grey"])) / 4, arr.ind = T), type = "bright"),
-                                    data.frame(which(acq[,,"grey"] < median(acq[,,"grey"]) / 2, arr.ind = T), type = "v.dim"),
-                                    data.frame(which(acq[,,"grey"] < median(acq[,,"grey"]) * 0.75, arr.ind = T), type = "dim"),
-                                        data.frame(which(acq[,,"black"] > median(acq[,,"black"]) + (65535 - median(acq[,,"black"])) / 2, arr.ind = T), type = "v.bright"),
-                                        data.frame(which(acq[,,"black"] > median(acq[,,"black"]) + (65535 - median(acq[,,"black"])) / 4, arr.ind = T), type = "bright"),
-                                        data.frame(which(acq[,,"black"] < median(acq[,,"black"]) / 2, arr.ind = T), type = "v.dim"),
-                                        data.frame(which(acq[,,"black"] < median(acq[,,"black"]) * 0.75, arr.ind = T), type = "dim"),
-                                    data.frame(which(find.lines(acq[, , "black"]) > 0, arr.ind = T), type = "line.b"),
-                                    data.frame(which(find.lines(acq[, , "black"], dim.lines = T) > 0, arr.ind = T), type = "line.d"),
-                                    data.frame(which(find.lines(acq[, , "black"], horizontal = T) > 0, arr.ind = T), type = "line.b"),
-                                    data.frame(which(find.lines(acq[, , "black"], dim.lines = T, horizontal = T) > 0, arr.ind = T), type = "line.d"),
-                                        data.frame(which(threshold(md.b[[x]], level = mad(pw.m[,,"black", x]) * 2) > 0, arr.ind = T), type = "l.bright")))
-    
-    bp <- apply(pw.m, 4,
-                function(img) edge.px(img))                 
-    # screen spots
-    # locally dim
-    # locally bright
-    qq <- apply(pw.m, 4, apply, 3, 
-                function(img) data.frame(which(img > median(img) + (65535 - median(img)) / 2, arr.ind = T)))
-    
-    qq <- data.frame(unique(rbind.fill(apply(pw.m[,,,"160430"], 3, function(img) data.frame(which(img > median(img) + (65535 - median(img)) / 2, arr.ind = T))))),
-                     type = "v.bright")
-    
-    data.frame(which(acq[,,"white"] > median(acq[,,"white"]) + (65535 - median(acq[,,"white"])) / 2, arr.ind = T), type = "v.bright"),
-    data.frame(which(acq[,,"white"] > median(acq[,,"white"]) + (65535 - median(acq[,,"white"])) / 4, arr.ind = T), type = "bright"),
-    data.frame(which(acq[,,"white"] < median(acq[,,"white"]) / 2, arr.ind = T), type = "v.dim"),
-    data.frame(which(acq[,,"white"] < median(acq[,,"white"]) * 0.75, arr.ind = T), type = "dim"),
+    bp.th <- apply(pw.m, 4, 
+                   function(acq) rbind(data.frame(unique(rbind.fill(apply(acq, 3, function(img) data.frame(which(img > median(img) + (65535 - median(img)) / 2, arr.ind = T))))),
+                                                  type = "v.bright"),
+                                       data.frame(unique(rbind.fill(apply(acq, 3, function(img) data.frame(which(img > median(img) + (65535 - median(img)) / 4, arr.ind = T))))),
+                                                  type = "bright"),
+                                       data.frame(unique(rbind.fill(apply(acq, 3, function(img) data.frame(which(img < median(img) / 2, arr.ind = T))))),
+                                                  type = "v.dim"),
+                                       data.frame(unique(rbind.fill(apply(acq, 3, function(img) data.frame(which(img < median(img) * 0.75, arr.ind = T))))),
+                                                  type = "dim")))
+    saveRDS(bp.th, paste0(fpath, "bad-px-thresholded-mean-values-incl-white.rds"))
 }
+bp <- merge.bp.lists(list(bp.th, 
+                          readRDS(paste0(fpath, "bad-px-constant.rds")),
+                          readRDS(paste0(fpath, "bad-px-bright-lines.rds")),
+                          readRDS(paste0(fpath, "bad-px-local-bright-dim-pixels.rds"))), cat.order = Cat)
+
+# any pixels marked as 'screen spot' are actually okay (after duplicates are removed), so filter these out
+# classification is needed intitially to discriminate between genuinely dim pixels and shadows from spot
+bp <- lapply(bp, function(bpx) bpx[bpx$type != "screen.spot",])
+
+# also not interested in edge pixels, so remove these as well
+bp <- lapply(bp, function(bpx) bpx[bpx$type != "edge",])
+
+
+# get transition matrix
+tr <- abind(get.transitions(bp), along = 3)
+
+# filter out categories that don't occur at all
+tr <- tr[colSums(apply(tr, 1:2, sum)) * rowSums(apply(tr, 1:2, sum)) > 0,
+         colSums(apply(tr, 1:2, sum)) * rowSums(apply(tr, 1:2, sum)) > 0,]
 
 # plot transitions as function of real time - are rates constant?
-{}
+{
+    # get matrix of all transitions, to see which actually occur
+    apply(tr, 1:2, sum)
+    
+    tr.prop <- array(apply(tr, 3, function(x) sweep(x, 1, rowSums(x), "/")), dim = dim(tr), dimnames = dimnames(tr))
+    
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "hot", to = "hot", ylim = c(0,1),
+                 main = "Transitions from hot state at each acquisition")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "hot", to = "v.bright", add = T, col = "red")
+    
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "v.bright", to = "v.bright", ylim = c(0,1),
+                 main = "Transitions from very bright state at each acquisition")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "v.bright", to = "hot", add = T, col = "red")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "v.bright", to = "bright", add = T, col = "blue")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "v.bright", to = "l.bright", add = T, col = "green3")
+    
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "l.bright", to = "l.bright", ylim = c(0,1),
+                 main = "Transitions from locally bright state at each acquisition")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "l.bright", to = "normal", add = T, col = "gold")
+    tr.prop.plot(tr.prop, as.Date(names(bp), "%y%m%d"), from = "l.bright", to = "bright", add = T, col = "red")
+    
+
+    tr.prop.slope(tr.prop, as.Date(names(bp), "%y%m%d"), from = "hot", to = "v.bright")
+}
+# no particular sign of rates worsening over time. Proportions remain reasonably constant.
+
+# check behaviour of each class under flat field correction
+# possible to make some sort of 3d plot in b/w/g 
 
 # check stability of classes
 {
@@ -75,14 +156,46 @@ Cat.cols <- c("purple", "black", "magenta3", "red", "orange", "gold", "yellow", 
 
 # cluster pixels according to path taken?
 {
-    # filter out all pixels that are always healthy
+    # get list of all pixels identified as unhealthy: 147132 pixels
+    qq <- rmerge.df.list(unique(rbind.fill(lapply(bp, "[", 1:2))), bp, by = c(1:2), all = T)
     
+    qq[,sapply(qq, is.factor)] <- lapply(qq[,sapply(qq, is.factor)], as.numeric)
+    qq[is.na(qq)] <- 0
+    qq$path <- apply(qq[,3:14], 1, paste, collapse = "")
     
-    # plot trajectory through state space
+    # identify static pixels
+    zz <- apply(qq[1:5,3:14], 1, function(r) all(sapply(r, function(x) x == r[1])))
     
+    # plot trajectories through state space
+    qq$path.type[which(apply(qq[,3:14], 1, function(r) all(r == r[1])))] <- "static"
+    table(qq$path[which(qq$path.type == "static")])
     
-    # cluster manually or by distance metric?
+    # check direction of movement through states
+    qq$zz <- apply(qq[,3:14], 1, function(vv) max(as.numeric(gsub(0,99, vv[2:12])) - as.numeric(gsub(0,99,vv[1:11]))))
+
+#********************************************************************************************    
     
+    # probably better to convert states so that 0 is at centre
+    
+    state.conv <- data.frame(c.name = c("normal", Cat), org = c(0:13),
+                        new = c(0,-4,-5,4,3,2,10,0,1,-10,0,-3,-2,-1))
+    state.conv[order(state.conv[,3]),]
+
+    hh <- apply(qq[,3:14], 2, function(r) state.conv$new[match(r, state.conv$org)])
+    
+    tt <- hh[,2:12] - hh[,1:11]
+    
+    tt.min <- apply(tt, 1, min)
+    
+    qq$path.type <- "unclassified"
+    qq$path.type[which(apply(tt, 1, min) == 0 & apply(hh, 1, min) == 0)] <- "increasing"
+    qq$path.type[which(apply(tt, 1, max) == 0 & apply(hh, 1, max) == 0)] <- "decreasing"
+    qq$path.type[which(rowSums(tt) == 0)] <- "static"
+    
+    count(qq$path.type)
+    
+    head(qq[qq$path.type == "decreasing",], 20)
+    head(qq[qq$path.type == "increasing",], 20)
     
 }
 
